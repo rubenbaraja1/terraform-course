f18062d54bb7c1b3f4dafe58578e8bfea76f5af9
rubenbaraja1 <88166714+rubenbaraja1@users.noreply.github.com>
2022-02-14 05:56:08 +0200

    Update configure-remote-state.sh

--- docker-demo-3/scripts/configure-remote-state.sh
+++ docker-demo-3/scripts/configure-remote-state.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Newer terraform versions have a new way of defining a backend. Copy paste the following code in a backend.tf file, modify the region/s3 bucket, and execute 'terraform init' to initialize the backend. You'll be asked to copy the data from the local backend to the s3 backend, which you can answer yes.
-"
-echo 'backend.tf
-==========
+touch backend.tf
+
+echo '
 terraform {
   backend "s3" {
     bucket = "terraform-state-a2b621920222022"
@@ -11,4 +10,6 @@ terraform {
     region = "eu-west-1"
   }
 }
-'
+' > backend.tf
+
+terraform init
